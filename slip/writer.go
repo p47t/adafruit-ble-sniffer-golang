2099401c07bb65f7d2090c9d4bc09c26ebe88ee5
Patrick Tsai <yinghau76@gmail.com>
2018-02-25 23:55:25 -0800

    Handle write error in SlipWriter

--- slip/writer.go
+++ slip/writer.go
@@ -13,20 +13,24 @@ func NewSlipWriter(wr io.Writer) *SlipWriter {
 	return &SlipWriter{bufio.NewWriter(wr)}
 }
 
-func (w *SlipWriter) Write(p []byte) (n int, err error) {
+func (w *SlipWriter) write(p ...byte) (nn int, err error) {
+	return w.wr.Write(p)
+}
+
+func (w *SlipWriter) Write(p []byte) (nn int, err error) {
 	for _, b := range p {
 		switch b {
 		case SLIP_START:
-			w.wr.WriteByte(SLIP_ESC)
-			w.wr.WriteByte(SLIP_ESC_START)
+			nn, err = w.write(SLIP_ESC, SLIP_ESC_START)
 		case SLIP_END:
-			w.wr.WriteByte(SLIP_ESC)
-			w.wr.WriteByte(SLIP_ESC_END)
+			nn, err = w.write(SLIP_ESC, SLIP_ESC_END)
 		case SLIP_ESC:
-			w.wr.WriteByte(SLIP_ESC)
-			w.wr.WriteByte(SLIP_ESC_ESC)
+			nn, err = w.write(SLIP_ESC, SLIP_ESC_ESC)
 		default:
-			w.wr.WriteByte(b)
+			nn, err = w.write(b)
+		}
+		if err != nil {
+			return
 		}
 	}
 	return len(p), nil
