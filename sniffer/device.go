33adf50f25ebbebccb6a41c5f5ce2dff93c6b3ee
Patrick Tsai <patrick.tsai@mediatek.com>
2018-02-26 13:54:59 -0800

    Store name and RSSI in Device

--- sniffer/device.go
+++ sniffer/device.go
@@ -1,7 +1,11 @@
 package sniffer
 
+import "fmt"
+
 type Device struct {
 	Address []byte
+	Name    string
+	RSSI    byte
 }
 
 func NewDevice(p *Packet) *Device {
@@ -9,7 +13,10 @@ func NewDevice(p *Packet) *Device {
 		return nil
 	}
 
+	addr := p.BlePacket.AdvAddr
 	return &Device{
-		Address: p.BlePacket.AdvAddr,
+		Address: addr,
+		Name:    fmt.Sprintf("%02x:%02x:%02x:%02x:%02x:%02x", addr[0], addr[1], addr[2], addr[3], addr[4], addr[5]),
+		RSSI:    p.RSSI,
 	}
 }
