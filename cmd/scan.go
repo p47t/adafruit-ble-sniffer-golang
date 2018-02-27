70b7fb2c808248c7cdc3b14fb71c53c0f5d8312d
Patrick Tsai <yinghau76@gmail.com>
2018-02-27 00:39:08 -0800

    Use duration type to specify scan duration

--- cmd/scan.go
+++ cmd/scan.go
@@ -8,8 +8,6 @@ import (
 	"github.com/yinghau76/adafruit-ble-sniffer-golang/sniffer"
 )
 
-var scanSeconds int32
-
 // scanCmd represents the scan command
 var scanCmd = &cobra.Command{
 	Use:   "scan",
@@ -19,8 +17,9 @@ var scanCmd = &cobra.Command{
 		s := sniffer.NewSniffer(portName)
 		defer s.Close()
 
-		log.Printf("scanning devices (%d s)...", scanSeconds)
-		devices, err := s.ScanDevices(time.Duration(scanSeconds) * time.Second)
+		scanSeconds, _ := cmd.Flags().GetDuration("duration")
+		log.Printf("scanning devices for %v", scanSeconds)
+		devices, err := s.ScanDevices(scanSeconds)
 		if err != nil {
 			log.Printf("failed to scan device: %v", err)
 			return
@@ -34,14 +33,5 @@ var scanCmd = &cobra.Command{
 
 func init() {
 	rootCmd.AddCommand(scanCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// scanCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	scanCmd.Flags().Int32VarP(&scanSeconds, "duration", "d", 5, "in seconds")
+	scanCmd.Flags().DurationP("duration", "d", 5*time.Second, `such as "5s" or "1m"`)
 }
