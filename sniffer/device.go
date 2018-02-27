aa2ab1bb1221281392780703f8bfa7cfdf1ff32f
Patrick Tsai <yinghau76@gmail.com>
2018-02-26 22:19:57 -0800

    Add TxAddType to Device

--- sniffer/device.go
+++ sniffer/device.go
@@ -1,22 +1,25 @@
 package sniffer
 
-import "fmt"
+import (
+	"fmt"
+)
 
 type Device struct {
-	Address []byte
-	Name    string
-	RSSI    byte
+	Address   []byte
+	TxAddType byte
+	Name      string
+	RSSI      byte
 }
 
 func NewDevice(p *Packet) *Device {
 	if p == nil || p.BlePacket == nil || len(p.BlePacket.AdvAddr) == 0 {
 		return nil
 	}
-
 	addr := p.BlePacket.AdvAddr
 	return &Device{
-		Address: addr,
-		Name:    fmt.Sprintf("%02x:%02x:%02x:%02x:%02x:%02x", addr[0], addr[1], addr[2], addr[3], addr[4], addr[5]),
-		RSSI:    p.RSSI,
+		Address:   addr,
+		TxAddType: p.BlePacket.TxAddType,
+		Name:      fmt.Sprintf("%02x:%02x:%02x:%02x:%02x:%02x", addr[0], addr[1], addr[2], addr[3], addr[4], addr[5]),
+		RSSI:      p.RSSI,
 	}
 }
